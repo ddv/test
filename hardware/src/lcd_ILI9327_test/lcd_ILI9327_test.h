1ff7ec186b005c92c319c614660c9bb1ef1d448a
ddv <derenok@gmail.com>
2016-01-10 14:21:15 +0500

    import test LCD and Megs test

---
+++ hardware/src/lcd_ILI9327_test/lcd_ILI9327_test.h
@@ -0,0 +1,25 @@
+// Only modify this file to include
+// - function definitions (prototypes)
+// - include files
+// - extern variable definitions
+// In the appropriate section
+
+
+#ifndef _lcd_ILI9327_test_H_
+#define _lcd_ILI9327_test_H_
+#include "Arduino.h"
+//add your includes for the project lcd_ILI9327_test here
+
+
+//end of add your includes here
+
+
+
+//add your function definitions for the project lcd_ILI9327_test here
+
+
+
+
+//Do not add code below this line
+#endif /* _lcd_ILI9327_test_H_ */
+
---
+++ hardware/src/lcd_ILI9327_test/spec.d
@@ -0,0 +1,2 @@
+spec.o: \
+ /home/ddv/workspace.luna/.metadata/.plugins/org.eclipse.cdt.managedbuilder.core/spec.C
